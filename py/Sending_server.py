48045426b8c5a15b47b8c26320c5cf434fb90464
OriHalyo <orihalyo123@gmail.com>
2018-05-14 11:39:02 +0300

    REFACTORING: menu

---
+++ py/Sending_server.py
@@ -0,0 +1,15 @@
+import socket
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+address =('localhost', 5002)
+
+sock.bind(address)
+
+while True:
+    data, addr = sock.recvfrom(1024)
+    print(data)
+    print(addr)
+    print("hey")
+
+
