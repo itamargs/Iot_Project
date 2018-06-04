a07102254552a4d4c3bd3271051618d159d6df7e
iotproject <orihalyo123@gmail.com>
2018-06-04 14:11:35 +0300

    Merging from master

--- py/device.py
+++ py/device.py
@@ -15,9 +15,11 @@ from time import sleep
 import dill as pickle
 import socket
 import os
+import time
 
 
 import zlib
+
 class device(ABC):
 
     def __init__(self, interval, ID, description):
@@ -143,41 +145,56 @@ class device(ABC):
 
     def sendData(path):
 
-        path = "filesAlreadyReduced"
 
         dirs = os.listdir(path)
 
-        filename, file_extension = os.path.splitext(path + "/" + dirs[0])
 
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        host = "127.0.0.1"
-        port  = 5002
-        sock.connect((host, port))
-
-
-        for files in dirs:
-            filename = files
-            size = len(filename)
-            size = bin(size)[2:].zfill(16) #encode file name to 16 bit
-            sock.sendall(size.encode('utf8'))
-            sock.sendall(filename.encode('utf8'))
-
-            filename = os.path.join(path,filename)
-            filesize = os.path.getsize(filename)
-            filesize = bin(filesize)[2:].zfill(32) # encode filesize as 32 bit binary
-            sock.sendall(filesize.encode('utf8'))
-
-            file_to_send = open(filename, 'rb')
-
-            l = file_to_send.read()
-            sock.sendall(l)
-            file_to_send.close()
+        while True:
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            host = "127.0.0.1"
+            port  = 5002
+            try:
+                sock.connect((host, port))
+            except:
+                print("Falied to connect, auto try again after 10sec")
+                time.sleep(10)
+                continue
 
 
 
 
-        sock.close()
+            for files in dirs:
+                filename = files
+                size = len(filename)
+                size = bin(size)[2:].zfill(16) #encode file name to 16 bit
+                sock.sendall(size.encode('utf8'))
+                sock.sendall(filename.encode('utf8'))
+
+                filename = os.path.join(path,filename)
+                filesize = os.path.getsize(filename)
+                filesize = bin(filesize)[2:].zfill(32) # encode filesize as 32 bit binary
+                sock.sendall(filesize.encode('utf8'))
+
+                file_to_send = open(filename, 'rb')
+
+                l = file_to_send.read()
+                sock.sendall(l)
+                file_to_send.close()
 
 
-    sendData('compressed.txt')
+
+
+            sock.close()
+
+        self.deleteFiles()
+
+
+    def deleteFiles(self, path):
+
+        filelist = [ f for f in os.listdir(path)]
+        for f in filelist:
+            os.remove(os.path.join(path, f))
+
+
+    sendData("filesAlreadyReduced")
 
