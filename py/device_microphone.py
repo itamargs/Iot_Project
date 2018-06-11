c661ac6455b6e7353d43fe651f0c0d0ed7257e57
itamar <itamargs@gmail.com>
2018-06-11 18:22:23 +0300

    FIX: file name format now as at should be ID-DATE-TIME.ext

--- py/device_microphone.py
+++ py/device_microphone.py
@@ -91,7 +91,7 @@ while(True):
 
         if case('first start'):
             print("case: first start")
-            myDevice = Device(10, 5645656656, "my  Microphone IoT device") #(self, interval, id, description)create device instance to actually run in background and gather data
+            myDevice = Device(1, "0001", "my  Microphone IoT device") #(self, interval, id, description)create device instance to actually run in background and gather data
             myDevice_ = myDevice.save('saved') #saving the device values to another sessions
 
             # myDevice.getReady()
@@ -117,12 +117,11 @@ while(True):
             print("case: new data")
             with open('saved', 'rb') as f:
                 myDevice = pickle.load(f)
-                # myDevice.sendDataToCloud()
             if myDevice.doesNeedAnalyzing() is True:
                 files = myDevice.getDataFromInputFolder("filesPool")  # get list of pointers to the files in the path provided folder
                 original_file_name = Path(files[0]).stem
                 filename123, original_file_extension = os.path.splitext(files[0])
-                date = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+                date = datetime.datetime.now().strftime("%d%m%Y-%H%M%S")
                 print("file name:" + original_file_name)
                 # myDevice.analyze()
                 # if myDevice.isTheDataHasChanged() is True:  # if there is a change
@@ -140,7 +139,7 @@ while(True):
                         # date = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
                         filename, file_extension = os.path.splitext(files[0])
                         shutil.move(files[0],
-                                         "readyFiles/" + original_file_name + "-" + date + file_extension)  # rename and move file to new folder
+                                         "readyFiles/" + myDevice.ID + "-" + date + file_extension)  # rename and move file to new folder
                     if myDevice.needCompression is True:
                         files = myDevice.getDataFromInputFolder("filesUnderProcess")
                         myDevice.compress(files, "readyFiles", original_file_name, date)
