77d895d75682766fd8d6a13e8ef2e6c25f086cf1
itamar <itamargs@gmail.com>
2018-06-20 02:53:15 +0300

    Update microphone to RC version

--- py/device_microphone.py
+++ py/device_microphone.py
@@ -74,8 +74,8 @@ while(True):
     if option == None:
         print("\nInsert case NUM:\n"
               " 1: first start\n"
-              " 2: new data (trigger received)\n"
-              " 3: interval activation (or ping from server)\n"
+              # " 2: new data (trigger received)\n"
+              # " 3: interval activation (or ping from server)\n"
               " 4: Stand By Mode")
         option = input("insert NUM: ")
         if option == "1":
@@ -90,10 +90,13 @@ while(True):
     for case in switch(option):
 
         if case('first start'):
-            print("case: first start")
-            myDevice = Device(1, "0001", "my  Microphone IoT device") #(self, interval, id, description)create device instance to actually run in background and gather data
+            print("\ncase: first start")
+            myDevice = Device(1, "0011", "my TensiometerIoT device") #(self, interval, id, description)create device instance to actually run in background and gather data
             myDevice_ = myDevice.save('saved') #saving the device values to another sessions
+            myDevice.printDetails()
+            print("--Sucess--")
 
+            '''
             # myDevice.getReady()
             # check if need start analyze data
             if myDevice.doesNeedAnalyzing() is True:
@@ -108,6 +111,7 @@ while(True):
                 else:  # if there is NO change
                     # myDevice.deleteOutdatedData()
                     myDevice.sendPulse()
+            '''
             option = None
             break
 
@@ -121,6 +125,7 @@ while(True):
                 files = myDevice.getDataFromInputFolder("filesPool")  # get list of pointers to the files in the path provided folder
                 original_file_name = Path(files[0]).stem
                 filename123, original_file_extension = os.path.splitext(files[0])
+                # todo: date won't change fast enogh if multiple files was forced copoid into input folder. can cause error in creating file  (won't be the case in production mode)
                 date = datetime.datetime.now().strftime("%d%m%Y-%H%M%S")
                 print("file name:" + original_file_name)
                 # myDevice.analyze()
@@ -128,34 +133,43 @@ while(True):
                 # myDevice.getChange()  # data has been changed so get the new data
                 # myDevice.compareData()
                 # reducing or compressing the files depends on their sensor settings
-                if myDevice.needReduction is True:
-                    myDevice.dataReduction(files, "filesUnderProcess")
-                    # date = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+                if myDevice.needReduction is True: # is the file need to go throw reduction process
+                    myDevice.dataReduction(files, "filesUnderProcess") # make reduction + save result in this path
                     filename, file_extension = os.path.splitext(files[0])
-                    shutil.move(files[0],
-                                "filesBeenCared/" + original_file_name + "-" + date + file_extension)  # rename and move file to new folder with the device supported file extension
+                    if myDevice.save_original_file is True:
+                        shutil.move(files[0],
+                                    "filesBeenCared/" + myDevice.ID + "-" + date + file_extension)  #
+
+                    try: # need to remove file from filesUnderProcess DIR. if we wanted to save original the file, the file wont be there so need to check that.
+                        os.remove(files[0])
+                    except FileNotFoundError:
+                        pass
+
                     if myDevice.needCompression is False:
                         files = myDevice.getDataFromInputFolder("filesUnderProcess")
-                        # date = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
                         filename, file_extension = os.path.splitext(files[0])
                         shutil.move(files[0],
                                          "readyFiles/" + myDevice.ID + "-" + date + file_extension)  # rename and move file to new folder
                     if myDevice.needCompression is True:
                         files = myDevice.getDataFromInputFolder("filesUnderProcess")
-                        myDevice.compress(files, "readyFiles", original_file_name, date)
+                        myDevice.compress(files, "readyFiles", myDevice.ID, date)
                         os.remove(files[0])
 
                 elif myDevice.needCompression is True:
                     files = myDevice.getDataFromInputFolder("filesPool")
-                    myDevice.compress(files, "readyFiles", original_file_name, date)
-                    # date = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
-                    shutil.move(files[0],
-                                "filesBeenCared/" + original_file_name + "-" + date + original_file_extension)
+                    myDevice.compress(files, "readyFiles", myDevice.ID, date) # compress file + save result in this path
+                    if myDevice.save_original_file is True:
+                        shutil.move(files[0],
+                                    "filesBeenCared/" + myDevice.ID + "-" + date + original_file_extension)
+                    try: # need to remove file from filePool DIR. if we wanted to save original file, the file wont be there so need to check that.
+                        os.remove(files[0])
+                    except FileNotFoundError:
+                        pass
 
 
                 # myDevice.deleteOutdatedData()
                 # myDevice.sendPulse()
-                # myDevice.sendData("readyFiles")  #Send all files inside path to the server
+                myDevice.sendData("readyFiles")  #Send all files inside path to the server
                 # waitForFileCreation()
             else:  # if there is NO change
                 myDevice.deleteOutdatedData()
@@ -185,7 +199,7 @@ while(True):
 
 
         if case('standBy'):
-            print("\nWelcome to project Ultron.\nStand by, We R Waiting for a trigger\n.")
+            print("\nWelcome to project Ultron.\nStand by, We R Waiting for new data\n.")
             while (True):
                 print("Searching for files in input directory...") #todo: implements other trigers then new file
                 filesExist = glob.glob("filesPool/*.*")  # create list of files in directory
@@ -203,6 +217,9 @@ while(True):
 
 
         if case(): # default, could also just omit condition or 'if True'
-            print("something else!")
+            # print("something else!")
+            pass
             # No need to break here, it'll stop anyway
+
+
 
